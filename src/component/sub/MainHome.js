0a5635b0d7aa3ec7c97e2629693687c97eb236ff
ElijahBus <elijahbus.ny@gmail.com>
2020-05-22 13:02:49 +0200

    add google maps on the homepage

---
+++ src/component/sub/MainHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// TODO:
+import HomeMap from '../services/Map';
+
+export default function Homepage() {
+    return (
+        <div className="main">
+            <HomeMap />
+        </div>
+    )
+}
