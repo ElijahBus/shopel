92e2dafde5b11af6775d3903e9e9ceb37439456e
ElijahBus <elijahbus.ny@gmail.com>
2020-06-11 13:47:53 +0200

    add landing shop page

--- src/component/sub/auth/Login.js
+++ src/component/sub/auth/Login.js
@@ -6,6 +6,7 @@ import {
   Input,  
   Button
  } from "reactstrap";
+ import {Link} from 'react-router-dom';
 
 function Login() {
   return( 
@@ -14,7 +15,9 @@ function Login() {
       <Input></Input>
       <Label>Password</Label>
       <Input type="password"></Input>
-      <Button>Login</Button>
+      <Link to="/shop">
+        <Button>Login</Button>
+      </Link>
     </Form>
   )
 }
